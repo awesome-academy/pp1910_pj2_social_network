efcf491fdf53746535e9ea0375a7a8b2619b1a0a
vanha2110 <vanha2110@gmail.com>
2020-07-15 20:29:37 +0700

    refs [Feature][PJ2-7] Create forgot password page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,43 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>{{ __('Login') }}</title>	
+		<link rel="icon" href="{{asset('assets/img/logo.jpg')}}">
+        <link type="text/css" href="{{asset('assets/css/demos/photo.css')}}" rel="stylesheet" />
+  </head>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<body>
+    <section class="login">
+        <div class="container">
+            <div class="banner-content">
+                <h1><i class="fa fa-smile"></i> {{ __('Fluffs') }}</h1>
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <form class="form-signin" method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <h3 class="form-signin-heading">{{ __('Forgot Password') }}</h3>
+                    <div class="form-group">
+                        <input id="email" name="email" type="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail Address">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <button class="kafe-btn kafe-btn-mint btn-block" type="submit">{{ __('Send Password Reset Link') }}</button>
+                </form>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </section> 
+
+	<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+	<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('assets/js/base.js')}}"></script>
+  </body>
+</html>
